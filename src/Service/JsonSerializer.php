3467a7e60f67e95652df41427b613d794771dc42
iradchenua <iradchenua@gmail.com>
2019-03-27 17:43:58 +0200

    forget to add jsonSerializer

---
+++ src/Service/JsonSerializer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: хм
+ * Date: 26.03.2019
+ * Time: 17:31
+ */
+
+namespace App\Service;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class JsonSerializer
+{
+    private $serializer;
+
+    public function __construct()
+    {
+        $encodes = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $this->serializer = new Serializer($normalizers, $encodes);
+    }
+
+    public function serialize($entityOnMap)
+    {
+        return $this->serializer->serialize($entityOnMap, 'json', [
+            'attributes' => [
+                'id',
+                'hullPoints',
+                'x',
+                'y',
+                'width',
+                'height',
+                'dirX',
+                'dirY',
+                'img',
+                'phase'
+            ]
+        ]);
+    }
+}
